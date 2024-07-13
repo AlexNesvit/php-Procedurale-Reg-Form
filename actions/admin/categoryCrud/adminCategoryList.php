5123cce88b739fce737a15b99d03d990478d46a7
AlexNesvit <alex.nesvit2000@gmail.com>
2024-07-13 23:07:57 +0200

    delete category

--- actions/admin/categoryCrud/adminCategoryList.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-require_once '../include/database.php';
-// liste tous les utilisateurs du site
-$req = $pdo->prepare('SELECT * FROM categorie');
-$req->execute();
-$categories = $req->fetchAll();
